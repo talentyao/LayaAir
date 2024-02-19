59f646821c3744ab3e7001367b0effbe3794177a
lirongf <619213582@qq.com>
2024-02-19 11:26:21 +0800

    add testTool

---
+++ src/layaAir/laya/d3/PerformanceTool.ts
@@ -0,0 +1,13 @@
+export function PERF_BEGIN(block: string): void {
+    //TODO
+}
+
+export function PERF_END(block: string): void {
+    //TODO
+}
+
+export class PerformanceDefine {
+    static SCENE3D_RENDER: string;
+    static SCENE3D_RENDERPRE: string;
+    static SCENE3D_RENDERCAM: string;
+}
