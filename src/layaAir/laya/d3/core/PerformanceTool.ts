7d5c6cdc5205223936952ef93ee8fc8bc24d8759
Michael <kellymalan@163.com>
2024-03-20 14:55:15 +0800

    Fixed issue # commit

--- src/layaAir/laya/d3/core/PerformanceTool.ts
+++ src/layaAir/laya/d3/core/PerformanceTool.ts
@@ -13,15 +13,39 @@ export class PerformanceDefine {
     static SCENE3D_RENDERCAM: string;
 }
 
+/**
+ * 性能统计开始
+ * @param block 统计标识（例如：PerformanceDefine.SCENE3D_RENDER）
+ * @constructor
+ */
 export function PERF_BEGIN(block: string): void {
     PerfTools.begin(block);
 }
 
+/**
+ * 性能统计结束
+ * @param block 统计标识（例如：PerformanceDefine.SCENE3D_RENDER）
+ * @constructor
+ */
 export function PERF_END(block: string): void {
     PerfTools.end(block);
 }
 
+/**
+ * 性能统计，每帧数量（例如：网络上传、下载量，资源加载、卸载量）
+ * @param tag     统计标识
+ * @param count   统计数量
+ * @constructor
+ */
+export function PERF_COUNT(tag:string,count:number) {
 
+}
+
+/**
+ * 性能统计装饰器，方法调用时间
+ * @param block 统计标识（例如：PerformanceDefine.SCENE3D_RENDER）
+ * @constructor
+ */
 export function PERF_STAT(block: string): MethodDecorator {
     return function (target: any, key: string | symbol, descriptor: PropertyDescriptor){
         const originalMethod = descriptor.value;
